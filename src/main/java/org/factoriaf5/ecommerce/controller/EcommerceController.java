632ed955bdc592d2760e5316592b882af56fab4a
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-11-28 18:55:42 +0100

    added more endpoint mappings to html templates

--- src/main/java/org/factoriaf5/ecommerce/controller/EcommerceController.java
+++ src/main/java/org/factoriaf5/ecommerce/controller/EcommerceController.java
@@ -1,14 +1,12 @@
 package org.factoriaf5.ecommerce.controller;
 
-import java.util.List;
-
-import org.factoriaf5.ecommerce.model.Product;
 import org.factoriaf5.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 @RequestMapping("ecommerce")
@@ -20,4 +18,16 @@ public class EcommerceController {
     public String home(){
         return "index";
     }
+    
+    @GetMapping("/products")
+    public String list() {
+        return "list";
+    }
+
+    @GetMapping("/products/create")
+    public String create() {
+        return "create";
+    }
+    
+    
 }
--- src/main/java/org/factoriaf5/ecommerce/service/ProductService.java
+++ src/main/java/org/factoriaf5/ecommerce/service/ProductService.java
@@ -1,7 +1,5 @@
 package org.factoriaf5.ecommerce.service;
 
-import java.util.List;
-
 import org.factoriaf5.ecommerce.model.Product;
 import org.factoriaf5.ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +10,23 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
-    public List<Product>getAllProducts(){
+    /* public List<Product>findAllProducts(){
         return productRepository.findAll();
+    } */
+
+    public Product findProductById(Long id){
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public void deleteProductById(Long id){
+        productRepository.deleteById(id);
+    }
+
+    public Product updateProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public Product createProduct(Product product){
+        return productRepository.save(product);
     }
 }
